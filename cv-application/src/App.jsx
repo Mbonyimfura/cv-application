dfae5cee0a84138ebbef6c52d8b420bb7ec117d7
Mbonyimfura <mbonyimfuratresor@gmail.com>
2023-10-28 19:21:11 +0200

    Add App.jsx and App.css

---
+++ cv-application/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import PersonalInformation from './components/Personal-information'
+import Summary from './components/Summary'
+import Education from './components/Education';
+import Experience from './components/Experience';
+function App() {
+  return (
+    <>
+  <PersonalInformation/>
+  <Summary/>
+  <Education/>
+<Experience/>
+    </>
+  )
+}
+
+export default App
